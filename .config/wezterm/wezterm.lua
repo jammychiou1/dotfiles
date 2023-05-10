d22b5b4654c10d5f79daf03667653625ea8e4277
jammychiou1 <jammy.chiou1@gmail.com>
2023-05-11 01:41:40 +0800

    Use csi u extended key, modify ssh config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,8 +5,8 @@ return {
     font = wezterm.font 'VictorMono Nerd Font',
     font_size = 9.0,
     -- color_scheme = 'Breeze',
-    enable_kitty_keyboard = true,
-    hide_tab_bar_if_only_one_tab = true,
+    enable_csi_u_key_encoding = true,
+    -- hide_tab_bar_if_only_one_tab = true,
     window_padding = {
         left = '0.5cell',
         right = '0.5cell',
@@ -17,12 +17,12 @@ return {
         { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
         { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
     },
-    -- ssh_domains = {
-    --     {
-    --         name = 'l8',
-    --         remote_address = '140.112.30.39',
-    --         username = 'b08902019',
-    --         default_prog = { 'zsh' },
-    --     },
-    -- },
+    ssh_domains = {
+        {
+            name = 'l8',
+            remote_address = '140.112.30.39',
+            username = 'b08902109',
+            default_prog = { 'zsh' },
+        },
+    },
 }
