78fd2159bfa43b5f9dc4397711c986a997cd1611
jammychiou1 <jammy.chiou1@gmail.com>
2024-05-04 13:39:51 +0800

    update wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+return {
+    font = wezterm.font_with_fallback { 'Hack Nerd Font Mono', 'Noto Sans Mono CJK TC' },
+    font_size = 10.0,
+    enable_csi_u_key_encoding = true,
+    -- hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = '0.5cell',
+        right = '0.5cell',
+        top = '0cell',
+        bottom = '0cell',
+    },
+    keys = {
+        { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
+        { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
+    },
+    ssh_domains = {
+        {
+            name = 'l8',
+            remote_address = '140.112.30.39',
+            username = 'b08902109',
+            default_prog = { 'zsh' },
+        },
+    },
+}
